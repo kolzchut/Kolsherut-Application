eb74070d5df4325af603915e900599400ee0fc74
joni <jonathan_a@webiks.com>
2025-06-17 10:20:16 +0300

    starting

--- FE/src/services/initialize/initialize.ts
+++ FE/src/services/initialize/initialize.ts
@@ -1,10 +1,8 @@
 import loadConfig from './loadConfig';
 import ReactDOM from 'react-dom/client';
 import React from "react";
-import testConnection from "../sendMessage/testConnection";
 
 export default async (main: React.ReactNode) => {
     await loadConfig();
-    await testConnection();
     ReactDOM.createRoot(document.getElementById('root')!).render(main);
 };
