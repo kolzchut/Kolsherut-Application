519d5af6ac36ec34a805ba72fc7d62258b25d93e
arielrepos <ariel_ohana@webiks.com>
2025-06-24 11:18:49 +0300

    Moved to Iprops

--- FE/src/pages/card/cardDetails/components/contact/contact.tsx
+++ FE/src/pages/card/cardDetails/components/contact/contact.tsx
@@ -1,16 +1,24 @@
 import Connection from "../../../../../components/connection/connection";
 import {BranchUrl} from "../../../../../types/cardType";
 
-const Contact = ({email, phoneNumbers, websites, address}: {email:string, phoneNumbers: string[], websites:BranchUrl[] | null, address:string}) =>{
+interface IProps {
+    email: string,
+    phoneNumbers: string[],
+    websites: BranchUrl[] | null,
+    address: string
+}
+
+const Contact = ({email, phoneNumbers, websites, address}: IProps) => {
     const contactTitle = window.strings.cardDetails.contactDetails
     console.log('email', email, 'phoneNumbers', phoneNumbers, 'websites', websites, 'address', address);
     return (
         <div>
             <span>{contactTitle}:</span>
-            {phoneNumbers.map((phoneNumber) => (<Connection type={`tel`} text={phoneNumber} key={phoneNumber} />))}
-            {email && (<Connection text={email} type={`mailto`} />)}
-            {websites && websites.map((website) => (<Connection type={`website`} text={website.title} link={website.href} key={website.href} />))}
-            {address && (<Connection text={address} type={`address`} />)}
+            {phoneNumbers.map((phoneNumber) => (<Connection type={`tel`} text={phoneNumber} key={phoneNumber}/>))}
+            {email && (<Connection text={email} type={`mailto`}/>)}
+            {websites && websites.map((website) => (
+                <Connection type={`website`} text={website.title} link={website.href} key={website.href}/>))}
+            {address && (<Connection text={address} type={`address`}/>)}
         </div>
     )
 }
