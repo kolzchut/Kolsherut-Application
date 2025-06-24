426119f48a472d560852d0b777fbb4f75d28d3c0
arielrepos <ariel_ohana@webiks.com>
2025-06-24 10:49:06 +0300

    Changed :cardId and :search to macro shape - %%cardId%% and %%search%%

--- FE/src/pages/card/cardLogic.ts
+++ FE/src/pages/card/cardLogic.ts
@@ -1,7 +1,7 @@
 import sendMessage from "../../services/sendMessage/sendMessage";
 
 export const getFullCard = async(cardId:string) => {
-    const requestURL = window.config.routes.card.replace(':cardId', cardId);
+    const requestURL = window.config.routes.card.replace('%%cardId%%', cardId);
     const response = await sendMessage({method:'get', requestURL})
     return response.success ? response.data : null;
 }
