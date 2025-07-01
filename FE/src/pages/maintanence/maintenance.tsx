29e9e475a7a03262c2554ca999c4dcaa18f720b0
jonathanAK <42270344+jonathanAK@users.noreply.github.com>
2025-07-01 11:11:45 +0300

    Update maintenance.tsx

--- FE/src/pages/maintanence/maintenance.tsx
+++ FE/src/pages/maintanence/maintenance.tsx
@@ -4,8 +4,7 @@ import maintenance from "../../assets/maintenance.png";
 
 const Maintenance = () => {
     const classes = useStyle();
-    //TODO: add attempt to go to main page
-    const home = location.protocol + '//' + location.host + location.pathname;
+    const home = location.protocol + '//' + location.host;
     setTimeout(()=>window.location.replace(home),2000);
     return <div className={classes.root}>
         <img src={maintenance} alt={'the page currently in maintenance'}/>
