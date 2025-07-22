43f5089cff7c9153fc220d186ba4fe36330d62e0
Ariel Ohana <Ariel_Ohana@webiks.com>
2025-07-22 13:04:14 +0300

    map (#59)
    * Analytics built, some of them still need implementations
    
    * feat: add debounce functionality for map bounds handling and update location filter
    
    * Merge
    
    * Update mapInteraction.ts
    
    ---------
    
    Co-authored-by: jonathanAK <42270344+jonathanAK@users.noreply.github.com>

--- FE/src/services/map/events/mapInteraction.ts
+++ FE/src/services/map/events/mapInteraction.ts
@@ -7,9 +7,19 @@ import {IMapInteractions, ViewInteractionEventTypes} from "../../../types/Intera
 import logger from "../../logger/logger";
 import {handler as hoverOnPOIHandler} from "./hoverOnPOI";
 import {mapDragEvent} from "../../gtag/mapEvents";
+import ILocation from "../../../types/locationType.ts";
+import {store} from "../../../store/store.ts";
+import {setLocationFilter} from "../../../store/filter/filterSlice.ts";
+import {transformExtent} from 'ol/proj';
 
 const globals = {
-    draggedBefore: false
+    draggedBefore: false,
+    lastSetNewLocationTimeOutId: 0
+}
+
+const debounce = ({cb,delay}:{cb:()=>void,delay:number}) => {
+    clearTimeout(globals.lastSetNewLocationTimeOutId);
+    globals.lastSetNewLocationTimeOutId = setTimeout(cb, delay);
 }
 
 const startDragEventIfNotHappenedBefore = (map: MapSingleton) => {
@@ -17,6 +27,31 @@ const startDragEventIfNotHappenedBefore = (map: MapSingleton) => {
     globals.draggedBefore = true;
     mapDragEvent(map.view.getZoom() || -1)
 }
+const handleNewBounds = (map: MapSingleton) => {
+    debounce({
+        cb: () => {
+            setNewLocationToStoreByBoundingBox(map);
+        },
+        delay: 500
+    });
+}
+
+const setNewLocationToStoreByBoundingBox = (map: MapSingleton) => {
+    const mapBounds = map.view.calculateExtent(map.ol.getSize()) as [number, number, number, number];
+
+    const bounds = transformExtent(
+        mapBounds,
+        map.view.getProjection().getCode(),
+        'EPSG:4326'
+    ) as [number, number, number, number];
+
+    const key = window.strings.map.locationByBoundingBox;
+    const newLocation: ILocation = {
+        key,
+        bounds,
+    }
+    store.dispatch(setLocationFilter(newLocation));
+};
 
 export const mapInteractions: IMapInteractions = [
     {
@@ -38,6 +73,7 @@ export const mapInteractions: IMapInteractions = [
         event: "pointerdrag",
         handler: (map: MapSingleton) => () => {
             startDragEventIfNotHappenedBefore(map);
+            handleNewBounds(map);
         }
     }
 ];
@@ -45,4 +81,17 @@ export const mapInteractions: IMapInteractions = [
 export const viewInteractions: Array<{
     event: ViewInteractionEventTypes,
     handler: (map: MapSingleton) => (event: ObjectEvent) => void
-}> = [];
+}> = [
+    {
+        event: 'change:resolution',
+        handler: (map: MapSingleton) => () => {
+            handleNewBounds(map);
+        }
+    },
+    {
+        event: 'change:rotation',
+        handler: (map: MapSingleton) => () => {
+            handleNewBounds(map);
+        }
+    }
+];
