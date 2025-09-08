e01649489554974e5a06862bca363f9d687572aa
arielrepos <ariel_ohana@webiks.com>
2025-09-08 18:08:11 +0300

    Add sitemap generation and taxonomy handling utilities WIP

--- be/src/controllers/sitemapControllers.ts
+++ be/src/controllers/sitemapControllers.ts
@@ -1,15 +1,15 @@
 import {Request, Response} from "express";
 import {asyncHandler} from "../middlewares/errorHandler";
-import getSiteMap from "../services/db/es/getSiteMap";
-import transformSiteMapFromJSONToXML from "../utilities/transformSiteMapFromJSONToXML";
 import recursiveTaxonomyToXML from "../utilities/taxonomy/recursiveTaxonomyToXML";
 import getTaxonomy from "../utilities/taxonomy/getTaxonomy";
 import breakdownTaxonomy from "../utilities/taxonomy/breakdownTaxonomy";
 import mixedTaxonomyToXML from "../utilities/taxonomy/mixedTaxonomyToXML";
+import {fetchAllCardFields} from "../services/db/es/getAllCardIds";
+import transformSiteMapFromJSONToXML from "../utilities/transformSiteMapFromJSONToXML";
 
 
 export const generalSitemap = asyncHandler(async (req: Request, res: Response) => {
-    const siteMap = await getSiteMap();
+    const siteMap = await fetchAllCardFields();
     const siteMapXML = transformSiteMapFromJSONToXML(siteMap);
     res.type('application/xml').status(200).send(siteMapXML);
 });
