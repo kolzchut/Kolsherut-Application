64d8c9ffd3ef52f8c3754a697d648777e1a5e9c4
arielrepos <ariel_ohana@webiks.com>
2025-09-10 12:35:02 +0300

    Built blacklist for mixedTaxonomy

--- be/src/utilities/taxonomy/mixedTaxonomyToXML.ts
+++ be/src/utilities/taxonomy/mixedTaxonomyToXML.ts
@@ -1,10 +1,13 @@
 import escapeXML from "../escapeXML";
 import vars from "../../vars";
+import mixedTaxonomyBlackListRaw from "../../assets/mixedTaxonomyBlackList.json";
 
 interface FixedTaxonomy {
     slug: string;
     query: string;
 }
+
+const mixedTaxonomyBlackList: Record<string, string[]> = mixedTaxonomyBlackListRaw;
 
 const buildLoc = ({response,situation}:{response: FixedTaxonomy,situation: FixedTaxonomy}): string => {
     return `${vars.serverSetups.origin}/?p=results&sq=${encodeURIComponent(response.query+ " "+situation.query)}&bsf=${encodeURIComponent(situation.slug)}&brf=${encodeURIComponent(response.slug)}`;
@@ -17,6 +20,7 @@ const mixedTaxonomyToXML = ({responses, situations}: {
     let xml = '';
     responses.forEach(response => {
         situations.forEach(situation => {
+            if (mixedTaxonomyBlackList[response.slug]?.includes(situation.slug)) return;
             xml += `<url>\n`;
             xml += `<loc>${escapeXML(buildLoc({response,situation}))}</loc>\n`;
             xml += `<priority>0.4</priority>\n`;
