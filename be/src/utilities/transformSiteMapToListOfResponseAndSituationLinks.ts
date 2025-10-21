5dee043b3dc139f8d3fac37d0217f8c77931a732
arielrepos <ariel_ohana@webiks.com>
2025-10-21 12:11:01 +0300

    Refactored URLS (in Sitemaps and Href in code) to fit the new structure

--- be/src/utilities/transformSiteMapToListOfResponseAndSituationLinks.ts
+++ be/src/utilities/transformSiteMapToListOfResponseAndSituationLinks.ts
@@ -10,12 +10,12 @@ const transformSiteMapToListOfResponseAndSituationLinks = ({responses,situations
 
     const responseUrls = responses.map(response =>({
         name: response.query || response.slug,
-        link:`${vars.serverSetups.origin}/?p=results&sq=${encodeForURL(response.query || response.slug)}&brf=${encodeForURL(response.slug)}`
+        link:`${vars.serverSetups.origin}/p/results/sq/${encodeForURL(response.query || response.slug)}/brf/${encodeForURL(response.slug)}`
     }));
 
     const situationsUrls = situations.map(situation => ({
         name: situation.query || situation.slug,
-        link:`${vars.serverSetups.origin}/?p=results&sq=${encodeForURL(situation.query || situation.slug)}&bsf=${encodeForURL(situation.slug)}`
+        link:`${vars.serverSetups.origin}/p/results/sq/${encodeForURL(situation.query || situation.slug)}/bsf/${encodeForURL(situation.slug)}`
     }));
 
     return {
