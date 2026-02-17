59a8f8f96b71295d31cfad46781817c465d4a8e4
arielrepos <ariel_ohana@webiks.com>
2026-02-17 11:37:11 +0200

    Enhance analytics and Hotjar initialization: prevent tracking for KolsherutBot and ensure proper initialization state

--- FE/src/services/hotjar/hotjar.ts
+++ FE/src/services/hotjar/hotjar.ts
@@ -3,11 +3,10 @@ import logger from "../logger/logger";
 
 
 const init = () => {
-    if (window.environment.state === "local") return;
+    if (navigator.userAgent.includes("KolsherutBot") || window.environment.state === "local") return;
     const {siteId, hotjarVersion} = window.config.hotjar;
     Hotjar.init(siteId, hotjarVersion);
     logger.log({message: `Initializing Hotjar on ${siteId} with version ${hotjarVersion}`});
 }
 
 export default {init}
-
