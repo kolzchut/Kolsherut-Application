a8f28b2dec474bcd3c658c15712b6288d89979a6
jonathanAK <42270344+jonathanAK@users.noreply.github.com>
2026-09-22 15:17:14 +0300

    Update hotjar.ts
    opt-out tag

--- FE/src/services/hotjar/hotjar.ts
+++ FE/src/services/hotjar/hotjar.ts
@@ -3,7 +3,7 @@ import logger from "../logger/logger";
 
 
 const init = () => {
-    if (navigator.userAgent.includes("KolsherutBot") || window.environment.state === "local") return;
+    if (navigator.userAgent.includes("KolSherutBot") || window.environment.state === "local") return;
     const {siteId, hotjarVersion} = window.config.hotjar;
     Hotjar.init(siteId, hotjarVersion);
     logger.log({message: `Initializing Hotjar on ${siteId} with version ${hotjarVersion}`});
